2443edd06e6f0abf349d211866efc8dff8245f79
David Grimbichler <david@grimbichler.ch>
2015-07-29 13:28:46 +0200

    Add Battery State
    Battery state is now shown with letter

--- src/main.c
+++ src/main.c
@@ -1,17 +1,38 @@
+/********************************************************************
+* Project:          Simple Electronic Watchface
+* Description:      Simple Watchface with a nice Background
+* Developer:        GrimbiXcode (David Grimbichler)
+* File:             main.c
+* First Release:    2015-07-29
+********************************************************************/
+
+
+/********************************************************************
+* Includes
+*
+********************************************************************/
 #include <pebble.h>
+#include <pebble_fonts.h>
   
   
 /********************************************************************
 * Variables
+*
 ********************************************************************/
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_battery_layer;
 static GBitmap *s_background_bitmap;
 static BitmapLayer *s_background_bitmap_layer;
 
 /********************************************************************
 * Routines
+*
 ********************************************************************/
+
+/*===================================================================
+* update time
+===================================================================*/
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
@@ -33,28 +54,49 @@ static void update_time() {
   text_layer_set_text(s_time_layer, buffer);
 }
 
+/*===================================================================
+* battery level handler
+===================================================================*/
+static void battery_handler(BatteryChargeState charge_state) {
+  static char battery_text[] = "100% charged";
+
+  if (charge_state.is_charging) {
+    snprintf(battery_text, sizeof(battery_text), "charging");
+  } else {
+    snprintf(battery_text, sizeof(battery_text), "%d%% charged", charge_state.charge_percent);
+  }
+  text_layer_set_text(s_battery_layer, battery_text);
+}
+
+/*===================================================================
+* time-tick handler
+===================================================================*/
 static void tick_handler(struct tm *tick_time, TimeUnits unit_changed){
   update_time();
 }
 
 /********************************************************************
 * Init-code
+*
 ********************************************************************/
+
+/*===================================================================
+* load main window
+===================================================================*/
 static void main_window_load(Window *window){
   // Create the Background
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds =layer_get_bounds(window_layer);
   
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ONE);
-  
   s_background_bitmap_layer = bitmap_layer_create(bounds);
+  
   bitmap_layer_set_bitmap(s_background_bitmap_layer, s_background_bitmap);
   #ifdef PBL_PLATFORM_APLITE
     bitmap_layer_set_compositing_mode(s_background_bitmap_layer, GCompOpAssign);
   #elif PBL_PLATFORM_BASALT
     bitmap_layer_set_compositing_mode(s_background_bitmap_layer, GCompOpSet);
   #endif 
-    layer_add_child(window_layer, bitmap_layer_get_layer(s_background_bitmap_layer));
   
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 100, 144, 50));
@@ -65,22 +107,44 @@ static void main_window_load(Window *window){
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  // Create battery TextLayer
+  s_battery_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  text_layer_set_background_color(s_battery_layer, GColorClear);
+  text_layer_set_text_color(s_battery_layer, GColorWhite);
+  text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
+  
+  // Improve the layout to be more like a battery state
+  //text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_12));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  // Add Layers as a child layer to the Window's root layer
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_bitmap_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
   
   // Make sure the time is displayed from the start
   update_time();
+  
+  // Get the current battery level
+  battery_handler(battery_state_service_peek());
 }
 
+/*===================================================================
+* unload main window
+===================================================================*/
 static void main_window_unload(Window *window){
   // Destroy Backgroundlayer
   bitmap_layer_destroy(s_background_bitmap_layer);
   gbitmap_destroy(s_background_bitmap);
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_battery_layer);
 }
 
+/*===================================================================
+* initialising Watchface
+===================================================================*/
 static void init(){
   // Create main Window element and assign to pointer
   s_main_window = window_create();
@@ -99,8 +163,14 @@ static void init(){
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Subscribe to the Battery State Service
+  battery_state_service_subscribe(battery_handler);
 }
 
+/*===================================================================
+* deinitialising Watchface
+===================================================================*/
 static void deinit(){
   // Destroy Window
   window_destroy(s_main_window);
